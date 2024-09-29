4542be37795387fafeea74342178432099013df7
prabu2389 <prabu23@gmail.com>
2024-09-28 23:02:17 -0400

    Added Base Class testNG config

--- src/test/java/org/example/pages/LoginPage.java
+++ src/test/java/org/example/pages/LoginPage.java
@@ -1,13 +1,10 @@
 package org.example.pages;
 
 import org.example.base.BddLibrary;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v85.page.Page;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
